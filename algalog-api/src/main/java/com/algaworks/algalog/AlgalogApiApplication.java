12e375beaf8fe80b3532d813768aafffe9a92067
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2022-11-04 22:31:04 +0000

    :sparkles: Implementando a classe controller

--- algalog-api/src/main/java/com/algaworks/algalog/AlgalogApiApplication.java
+++ algalog-api/src/main/java/com/algaworks/algalog/AlgalogApiApplication.java
@@ -1,4 +1,4 @@
-package com.algaworks.algalog.algalogapi;
+package com.algaworks.algalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
