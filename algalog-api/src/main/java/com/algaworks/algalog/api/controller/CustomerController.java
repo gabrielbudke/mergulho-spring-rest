7fba80591cf5cdb3287e259b5d5a348778e63172
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2022-11-05 20:30:40 +0000

    :sparkles: Implementando model de clientes

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/CustomerController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/CustomerController.java
@@ -1,14 +1,25 @@
 package com.algaworks.algalog.api.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algalog.domain.model.Customer;
 
 @RestController
 public class CustomerController {
     
     @GetMapping("/customers")
-    public String listar() {
-        return "Hello AlgaWorks";
+    public List<Customer> listar() {
+        Customer customer = new Customer();
+        customer.setId(1L);
+        customer.setName("João");
+        customer.setEmail("joao@algalog.com.br");
+        customer.setTelefone("47 98787-6767");
+        
+        return Arrays.asList(customer);
     }
 
 }
