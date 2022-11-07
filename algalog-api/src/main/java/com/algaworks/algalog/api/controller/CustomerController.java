6b0d329eb79f3a411ab6f0010e4681c6f51fdfdc
Gabriel Budke de Sousa <gabrielbudke@gmail.com>
2022-11-07 21:21:03 +0000

    :sparkles: Implementando as rotas de create, update e read

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/CustomerController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/CustomerController.java
@@ -1,33 +1,73 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Customer;
+import com.algaworks.algalog.domain.repository.CustomerRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
+
+    private CustomerRepository customerRepository;
     
-    @GetMapping("/customers")
-    public List<Customer> listar() {
+    @GetMapping
+    public List<Customer> readCustomers() {
+        return customerRepository.findAll();
+    }
 
-        var firstCustomer = new Customer();
-        firstCustomer.setId(1L);
-        firstCustomer.setName("João");
-        firstCustomer.setEmail("joao@algalog.com.br");
-        firstCustomer.setPhoneNumber("4798787-6767");
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Customer> readCustomerById(@PathVariable Long customerId) {
+        return customerRepository.findById(customerId)
+            .map(customer -> ResponseEntity.ok(customer))
+            .orElse(ResponseEntity.notFound().build());
 
-        var secondCustomer = new Customer();
-        secondCustomer.setId(2L);
-        secondCustomer.setName("Maria");
-        secondCustomer.setEmail("maria@algalog.com.br");
-        secondCustomer.setPhoneNumber("4798585-4545");  
+    }
 
-        return Arrays.asList(firstCustomer, secondCustomer);
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
 
+    @PutMapping("/{customerId}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long customerId, @RequestBody Customer customer) {
+        if(!customerRepository.existsById(customerId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        customer.setId(customerId);
+        customer = customerRepository.save(customer);
+
+        return ResponseEntity.ok(customer);
+
+    }
+
+    @DeleteMapping("/{customerId}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long customerId) {
+        if(!customerRepository.existsById(customerId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        customerRepository.deleteById(customerId);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
--- algalog-api/src/main/java/com/algaworks/algalog/domain/model/Customer.java
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/model/Customer.java
@@ -1,15 +1,33 @@
 package com.algaworks.algalog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
+@Table(name="customer")
 public class Customer {
 
+    @EqualsAndHashCode.Include
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
+
+    @Column(name="phoneNumber")
     private String phoneNumber;
+
+    
     
 }
